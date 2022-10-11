b3fee54a20038f78274b52a21c83777e12d4557a
Gilles Poncelet <gponcele@fu-r4-p4.s19.be>
2022-10-11 14:57:41 +0200

    MAJ

--- libft/ft_success.c
+++ libft/ft_success.c
@@ -6,7 +6,7 @@
 /*   By: gponcele <gponcele@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 17:04:48 by gponcele          #+#    #+#             */
-/*   Updated: 2022/10/11 14:45:34 by gponcele         ###   ########.fr       */
+/*   Updated: 2022/10/11 14:55:31 by gponcele         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ void	success2(char *msg, t_list *a, t_list *b)
 {
 	ft_putendl_fd(msg, 1);
 	ft_free(a);
+	free(a->first);
+	free(a);
 	free(b);
 	exit(EXIT_SUCCESS);
 }
